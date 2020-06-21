2672000f73ce362fa49a18a4c649ad3bcf09b331
hockeymonday <adamakadhim@gmail.com>
2020-06-21 04:03:17 -0400

    smfh

--- app/src/main/java/com/example/tinylaw/MainActivity.java
+++ app/src/main/java/com/example/tinylaw/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.tinylaw;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
