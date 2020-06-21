699748f5a7dc9fe5292cb2f57a0e08c5e6477348
vravich <vravich@ncsu.edu>
2020-06-21 14:10:54 -0400

    Created EmergencyActivity and began writing its logic

--- app/src/main/java/com/example/tinylaw/EmergencyActivity.java
+++ app/src/main/java/com/example/tinylaw/EmergencyActivity.java
@@ -1,4 +1,71 @@
 package com.example.tinylaw;
 
-public class EmergencyActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EmergencyActivity extends AppCompatActivity {
+
+    //Fields and constants
+    private TextView mTextView;
+    private ContentResolver contentResolver;
+    private Window window;
+    private int userBrightness;
+    private final int brightness = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Initial state
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+        mTextView = findViewById(R.id.text);
+        //Starting emergency response feature by checking settings permission
+        if (checkSystemWritePermission()) {
+            contentResolver = getContentResolver();
+            window = getWindow();
+            //Turns off auto-brightness
+            try {
+                Settings.System.putInt(contentResolver,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            } catch (Exception e) {
+                Log.e("Error", "No access");
+            }
+            //Stores user's current brightness setting
+            userBrightness = (int)window.getAttributes().screenBrightness;
+            Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+            WindowManager.LayoutParams layout = window.getAttributes();
+            layout.screenBrightness = brightness;
+            //Sets minimum brightness
+            window.setAttributes(layout);
+        }
+    }
+
+    private boolean checkSystemWritePermission() {
+        boolean retVal = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            retVal = Settings.System.canWrite(this);
+            Log.d("Permission:", "Can Write Settings: " + retVal);
+            if(retVal){
+                Toast.makeText(this, "Write allowed :-)", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Write not allowed :-(", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                startActivity(intent);
+            }
+        }
+        return retVal;
+    }
 }
+
