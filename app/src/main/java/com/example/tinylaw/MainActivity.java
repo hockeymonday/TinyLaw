c21b5fcbbdc58cbe7f3db04d644237ab8aa4b41f
hockeymonday <adamakadhim@gmail.com>
2020-06-21 01:59:42 -0400

    deez

--- app/src/main/java/com/example/tinylaw/MainActivity.java
+++ app/src/main/java/com/example/tinylaw/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.tinylaw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.constraint);
+        imageView = findViewById(R.id.image);
+
+
     }
 }
