254d8b37b087d6edcf67f1f05bbc47618eeafa73
scedanquah <62679434+scedanquah@users.noreply.github.com>
2020-06-21 08:01:59 -0400

    started parsing, final json format

--- app/src/main/java/com/example/tinylaw/MainActivity.java
+++ app/src/main/java/com/example/tinylaw/MainActivity.java
@@ -11,6 +11,13 @@ import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button emergency;
@@ -47,5 +54,40 @@ public class MainActivity extends AppCompatActivity {
         }
         getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).commit();
 
+    }
+
+//Call English JSON
+    public String loadJSONFromAsset() {
+        String json = null;
+        try{
+            InputStream is = getAssets().open("englishLaws.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    try{
+        JSONObject jsonObject = new JSONObject(loadJSONFromAsset());
+        JSONObject info = jsonObject.getJSONObject("info");
+        JSONArray infoList = info.getJSONArray("categories");
+
+
+        //collect name of each category
+        final String[] categoryNames = new String[0];
+        for(int i=0; i<infoList.length(); i++) {
+            String categoryName = infoList.getJSONObject(i).getString("name");
+            categoryNames[i] = categoryName;
+        }
+    }
+    catch{
+        //insert catch
     }
 }
