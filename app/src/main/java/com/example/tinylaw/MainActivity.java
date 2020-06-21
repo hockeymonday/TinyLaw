c61d245f34af0d364c0ac126948e14b8fa2623df
hockeymonday <adamakadhim@gmail.com>
2020-06-21 06:51:05 -0400

    smhhh

--- app/src/main/java/com/example/tinylaw/MainActivity.java
+++ app/src/main/java/com/example/tinylaw/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.tinylaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button emergency;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.ui);
         emergency = findViewById(R.id.emergency);
+        searchView = findViewById(R.id.search_bar);
+        searchView.setQueryHint("Search for a Right");
 
         //Emergency button activation
         Button emergency = findViewById(R.id.emergency);
@@ -27,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(emergencyActivityIntent);
             }
         });
+
+
+        // Run Setup Activity on First App Open
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+        if (isFirstRun) {
+            //show start activity
+            startActivity(new Intent(MainActivity.this, SetupActivity.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).commit();
 
     }
 }
---
+++ app/src/main/java/com/example/tinylaw/SetupActivity.java
@@ -0,0 +1,50 @@
+package com.example.tinylaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    String[] languages = {"English", "Spanish", "Chinese", "Arabic", "Korean", "French"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+        spinner = findViewById(R.id.spin);
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> languages = new ArrayList<String>();
+        languages.add("English");
+        languages.add("Español");
+        languages.add("Français");
+        languages.add("العربية");
+        languages.add("普通话");
+        languages.add("한국인");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,languages);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),"Selected: "+item,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
