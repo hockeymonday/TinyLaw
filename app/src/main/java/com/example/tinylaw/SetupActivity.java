d3acb96f0bbccc597541de80a8053d2223a78d2f
hockeymonday <adamakadhim@gmail.com>
2020-06-21 14:36:36 -0400

    smhhh

--- app/src/main/java/com/example/tinylaw/SetupActivity.java
+++ app/src/main/java/com/example/tinylaw/SetupActivity.java
@@ -3,11 +3,17 @@ package com.example.tinylaw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,7 +21,13 @@ import java.util.List;
 
 public class SetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
-    String[] languages = {"English", "Spanish", "Chinese", "Arabic", "Korean", "French"};
+    Button button;
+    String item;
+    Button contacts;
+    String contactnum;
+    String contactname;
+    Intent intent;
+    TextView contact_display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,11 @@ public class SetupActivity extends AppCompatActivity implements AdapterView.OnIt
         setContentView(R.layout.activity_setup);
         spinner = findViewById(R.id.spin);
         spinner.setOnItemSelectedListener(this);
+        button = findViewById(R.id.but);
+        contacts = findViewById(R.id.select_contact_but);
+        contact_display = findViewById(R.id.contact);
+
+        intent = new Intent(getApplicationContext(), MainActivity.class);
 
         List<String> languages = new ArrayList<String>();
         languages.add("English");
@@ -32,23 +49,63 @@ public class SetupActivity extends AppCompatActivity implements AdapterView.OnIt
         languages.add("普通话");
         languages.add("한국인");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,languages);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("language", item);
+                Log.d("deez", item);
+                startActivity(intent);
+            }
+        });
+
+        contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                pickContact.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(pickContact, 1);
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String item = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),"Selected: "+item,Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this,MainActivity.class);
-        intent.putExtra("language",item);
-        startActivity(intent);
+        item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Uri contactData = data.getData();
+                Cursor cursor = managedQuery(contactData, null, null, null, null);
+                cursor.moveToFirst();
+
+                String number = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                String name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+                contactname = name;
+                contactnum = number;
+
+                contact_display.setText(name+", "+number);
+
+                intent.putExtra("number", number);
+                intent.putExtra("name", name);
+            }
+        }
     }
 }
