4e042d785c1152d393327924a7a96f6b9fd17176
scedanquah <62679434+scedanquah@users.noreply.github.com>
2020-06-21 18:46:06 -0400

    continuing PoliceActivity (not sure what to do with emergency)

--- app/src/main/java/com/example/tinylaw/PoliceActivity.java
+++ app/src/main/java/com/example/tinylaw/PoliceActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SearchView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,12 +16,24 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PoliceActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button policeButton1;
+    Button policeButton2;
+    Button policeButton3;
+    Button policeButton4;
+    Button policeButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_police);
 
+        policeButton1 = findViewById(R.id.policeButton1);
+        policeButton2 = findViewById(R.id.policeButton2);
+        policeButton3 = findViewById(R.id.policeButton3);
+        policeButton4 = findViewById(R.id.policeButton4);
+        policeButton5 = findViewById(R.id.policeButton5);
+        imageView = findViewById(R.id.ui);
         // Get the string as an array list
         Intent intent = getIntent();
         String jsonArray = intent.getStringExtra("police");
